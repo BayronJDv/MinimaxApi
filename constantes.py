65ff894cc132aa4586abfe0951f84423e434e8b1
unknown <jfuertes1213@gmail.com>
2025-05-25 23:13:35 -0500

    Se corrige error en ZonaC

--- constantes.py
+++ constantes.py
@@ -1,7 +1,7 @@
 # Definición de zonas especiales del mapa
 zonaA = [(0,0),(0,1),(0,2),(1,0),(2,0)]
 ZonaB = [(0,5),(0,6),(0,7),(1,7),(2,7)]
-ZonaC = [(0,5),(0,6),(0,7),(1,7),(2,7)]
+ZonaC = [(5,0),(6,0),(7,0),(7,1),(7,2)]
 ZonaD = [(5,7),(6,7),(7,7),(7,5),(7,6)]
 
 # Lista de todas las zonas
